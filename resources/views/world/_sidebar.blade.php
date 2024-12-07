fbb9876206ba5d9bb11a7c95bce4d98babf1eeaf
min <liwoyadans@gmail.com>
2024-12-07 15:10:57 -0800

    chore: just adding world sidebar links

--- resources/views/world/_sidebar.blade.php
+++ resources/views/world/_sidebar.blade.php
@@ -8,6 +8,7 @@
         <div class="sidebar-item"><a href="{{ url('world/trait-categories') }}" class="{{ set_active('world/trait-categories*') }}">Trait Categories</a></div>
         <div class="sidebar-item"><a href="{{ url('world/traits') }}" class="{{ set_active('world/traits*') }}">All Traits</a></div>
         <div class="sidebar-item"><a href="{{ url('world/character-categories') }}" class="{{ set_active('world/character-categories*') }}">Character Categories</a></div>
+        <div class="sidebar-item"><a href="{{ url('world/character-classes') }}" class="{{ set_active('world/character-classes*') }}">Character Classes</a></div>
         <div class="sidebar-item"><a href="{{ url('world/elements') }}" class="{{ set_active('world/elements*') }}">All Elements</a></div>
     </li>
     <li class="sidebar-section">
@@ -15,6 +16,7 @@
         <div class="sidebar-item"><a href="{{ url('world/item-categories') }}" class="{{ set_active('world/item-categories*') }}">Item Categories</a></div>
         <div class="sidebar-item"><a href="{{ url('world/items') }}" class="{{ set_active('world/items*') }}">All Items</a></div>
         <div class="sidebar-item"><a href="{{ url('world/currencies') }}" class="{{ set_active('world/currencies*') }}">Currencies</a></div>
+        <div class="sidebar-item"><a href="{{ url('world/pet-categories') }}" class="{{ set_active('world/pet-categories*') }}">Pet Categories</a></div>
         <div class="sidebar-item"><a href="{{ url('world/pets') }}" class="{{ set_active('world/pets*') }}">Pets</a></div>
     </li>
     <li class="sidebar-section">
@@ -24,6 +26,8 @@
     </li>
     <li class="sidebar-section">
         <div class="sidebar-section-header">Claymore</div>
+        <div class="sidebar-item"><a href="{{ url('world/skill-categories') }}" class="{{ set_active('world/skill-categories*') }}">Skill Categories</a></div>
+        <div class="sidebar-item"><a href="{{ url('world/skills') }}" class="{{ set_active('world/skills*') }}">All Skills</a></div>
         <div class="sidebar-item"><a href="{{ url('world/weapon-categories') }}" class="{{ set_active('world/weapon-categories*') }}">Weapon Categories</a></div>
         <div class="sidebar-item"><a href="{{ url('world/weapons') }}" class="{{ set_active('world/weapons*') }}">All Weapons</a></div>
         <div class="sidebar-item"><a href="{{ url('world/gear-categories') }}" class="{{ set_active('world/gear-categories*') }}">Gear Categories</a></div>
