d5c557acbb09d0faad11c5042d1348092c6232d6
Anders <carousel.court@outlook.com>
2025-10-26 13:05:59 -0500

    no message

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -114,12 +114,12 @@
         @include('character._tab_skills', ['character' => $character, 'skills' => $skills])
 		</div>
         <div class="col-6 p-1 card">
-        <div class="row justify-content-center no-gutters">
+        <div class="row align-items-center justify-content-between no-gutters">
         @foreach ($character->stats->chunk(4) as $chunk)
-                <div class="row justify-content-center no-gutters">
+                <div class="row align-items-center justify-content-between no-gutters">
                     @foreach ($chunk as $stat)
                     
-                        <div class="p-3 m-4 rounded p-4 stat-entry" style="max-width: 200px;background-color: {{ $stat->stat->colour }};" data-id="{{ $stat->id }}">
+                        <div class="p-1 m-2 rounded p-2 stat-entry" style="max-width: 200px;background-color: {{ $stat->stat->colour }};" data-id="{{ $stat->id }}">
                             <h6 class="text-center" style=" text-transform:uppercase;">
                                 {{ $stat->stat->name }}
                                 <br>
