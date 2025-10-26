da1a9003a4bc6a1ea1a1479f5588ab43ab599f69
Anders <carousel.court@outlook.com>
2025-10-26 10:42:38 -0500

    no message

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -57,7 +57,7 @@
 
     <div class="container character-bio col-12 d-flex">
 	<div class="row align-items-start d-flex">
-		<div class="col-md-4 pr-4">
+		<div class="col pr-4">
         @if (count($image->character->pets))
                     <div class="row justify-content-center text-center">
                         {{-- get one random pet --}}
@@ -83,7 +83,7 @@
                 @endif
 		</div>
 
-		<div class="col-md-2 card">
+		<div class="col card">
         @if (count($image->character->equipment()))
                     <div class="mb-1 mt-4">
                         <div class="mb-0">
@@ -108,7 +108,7 @@
                     </div>
                 @endif
         </div>
-		<div class="col-md-3 card">
+		<div class="col card">
             <div class="row">
                     <div class="col-lg-5 col-md-6 col-3">
                         <h5>Background</h5>
@@ -143,10 +143,10 @@
 
     
     <div class="row align-items-start p-3 d-flex">
-		<div class="col-md-6 card m-2 p-2">
+		<div class="col card m-2 p-2">
         @include('character._tab_skills', ['character' => $character, 'skills' => $skills])
 		</div>
-        <div class="col-md-6 card m-2 p-2">
+        <div class="col card m-2 p-2">
         <div class="row">
         @foreach ($character->stats->chunk(4) as $chunk)
                 <div class="row justify-content-center no-gutters">
