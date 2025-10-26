639c8d950096d79ceaca3f075d2d93c9c8a12ec8
Anders <carousel.court@outlook.com>
2025-10-26 09:32:01 -0500

    mexp3

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -85,7 +85,7 @@
 
 		<div class="col-md-6 card">
         @if (count($image->character->equipment()))
-                    <div class="mb-1 mt-4">
+
                         <div class="mb-0">
                             <h5>Equipment</h5>
                         </div>
@@ -105,7 +105,6 @@
                         <div class="float-right">
                             <a href="{{ $character->url . '/stats' }}">View All...</a>
                         </div>
-                    </div>
                 @endif
             </div>
 		</div>
@@ -113,7 +112,7 @@
         </div>
     
     <div class="row align-items-start">
-		<div class="col-4 card m-3">
+		<div class="col-4 card">
         <div class="row">
                     <div class="col-lg-5 col-md-6 col-3">
                         <h5>Background</h5>
@@ -149,7 +148,7 @@
                     <div class="col-lg-7 col-md-6 col-8">{!! $image->species_id ? $image->species->displayName : 'None' !!}</div>
                 </div>
 		</div>
-        <div class="col-8 card m-3">
+        <div class="col-8 card">
         <div class="row">
         @foreach ($character->stats->chunk(4) as $chunk)
                 <div class="row justify-content-center no-gutters">
