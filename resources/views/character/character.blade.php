4c956cf08adfdefb2f3a530a9f0957861f22345a
Anders <carousel.court@outlook.com>
2025-10-26 10:22:42 -0500

    sm3

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -83,7 +83,7 @@
                 @endif
 		</div>
 
-		<div class="col-md-5 card">
+		<div class="col-md-4 card">
         @if (count($image->character->equipment()))
                     <div class="mb-1 mt-4">
                         <div class="mb-0">
@@ -113,7 +113,7 @@
         </div>
     
     <div class="row align-items-start p-3 d-flex">
-		<div class="col-md-6 card m-2 p-2">
+		<div class="col-md-3 card m-2 p-2">
         @include('character._tab_skills', ['character' => $character, 'skills' => $skills])
 		</div>
         <div class="col-md-6 card m-2 p-2">
