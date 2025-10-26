4ef5ebf595a70c45bf7876336d26927703976954
Anders <carousel.court@outlook.com>
2025-10-26 08:21:03 -0500

    exp4

--- resources/views/character/character.blade.php
+++ resources/views/character/character.blade.php
@@ -54,6 +54,118 @@
         @include('character._image_info', ['image' => $character->image])
     </div>
 
+    <div class="container-fluid character-bio" id="info-{{ $image->id }}">
+	<div class="row">
+		<div class="col-md-4">
+
+        @if (count($image->character->pets))
+                    <div class="row justify-content-center text-center">
+                        {{-- get one random pet --}}
+                        @php
+                            $pets = $image->character
+                                ->pets()
+                                ->orderBy('sort', 'DESC')
+                                ->limit(config('lorekeeper.pets.display_pet_count'))
+                                ->get();
+                        @endphp
+                        @foreach ($pets as $pet)
+                            @if (config('lorekeeper.pets.pet_bonding_enabled'))
+                                @include('character._pet_bonding_info', ['pet' => $pet])
+                            @else
+                                <div class="ml-2 mr-3">
+                                    <img src="{{ $pet->pet->variantImage($pet->id) }}" style="max-width: 75px;" />
+                                    <br>
+                                    <span class="text-light badge badge-dark" style="font-size:95%;">{!! $pet->pet_name !!}</span>
+                                    </div>
+                                @endif
+                        @endforeach
+                        <div class="ml-auto float-right mr-3">
+                            <a href="{{ $character->url . '/pets' }}" class="btn btn-outline-info btn-sm">View All</a>
+                        </div>
+                    </div>
+                @endif
+		</div>
+		<div class="col-md-4">
+        @if (count($image->character->equipment()))
+                    <div class="mb-1 mt-4">
+                        <div class="mb-0">
+                            <h5>Equipment</h5>
+                        </div>
+                        <div class="text-center row">
+                            @foreach ($image->character->equipment()->take(5) as $equipment)
+                                <div class="col-md-2">
+                                    @if ($equipment->has_image)
+                                        <img class="rounded" src="{{ $equipment->imageUrl }}" data-toggle="tooltip" title="{{ $equipment->equipment->name }}" style="max-width: 75px;" />
+                                    @elseif($equipment->equipment->imageurl)
+                                        <img class="rounded" src="{{ $equipment->equipment->imageUrl }}" data-toggle="tooltip" title="{{ $equipment->equipment->name }}" style="max-width: 75px;" />
+                                    @else
+                                        {!! $equipment->equipment->displayName !!}
+                                    @endif
+                                </div>
+                            @endforeach
+                        </div>
+                        <div class="float-right">
+                            <a href="{{ $character->url . '/stats' }}">View All...</a>
+                        </div>
+                    </div>
+                @endif
+            </div>
+
+		</div>
+		<div class="col-md-4">
+        <div class="row">
+                    <div class="col-lg-5 col-md-6 col-3">
+                        <h5>Background</h5>
+                    </div>
+                    <div class="col-lg-7 col-md-6 col-8">{!! $image->character->class_id ? $image->character->class->displayName : 'None' !!}
+                        @if (Auth::check())
+                            @if (Auth::user()->isStaff == $image->character->user_id && $image->character->class_id == null)
+                                <a href="#" class="btn btn-outline-info btn-sm edit-class ml-1" data-id="{{ $image->character->id }}"><i class="fas fa-cog"></i></a>
+                            @endif
+                        @endif
+                    </div>
+                </div>
+                @if ($image->transformation_id)
+                    <div class="row">
+                        <div class="col-lg-5 col-md-6 col-4">
+                            <h5>Form {!! add_help('The main image is always the active image') !!}</h5>
+                        </div>
+                        <div class="col-lg-7 col-md-6 col-8">
+                            <a href="{{ $image->transformation->url }}">
+                                {!! $image->transformation->displayName !!}
+                            </a>
+                        </div>
+                    </div>
+                @endif
+                @if ($image->character->homeSetting)
+                    <div class="row">
+                        <div class="col-lg-5 col-md-6 col-4">
+                            <h5>Home</h5>
+                        </div>
+                        <div class="col-lg-7 col-md-6 col-8">{!! $image->character->location ? $image->character->location : 'None' !!}</div>
+                    </div>
+                @endif
+                @if ($image->character->factionSetting)
+                    <div class="row">
+                        <div class="col-lg-5 col-md-6 col-4">
+                            <h5>Faction</h5>
+                        </div>
+                        <div class="col-lg-7 col-md-6 col-8">{!! $image->character->faction ? $image->character->currentFaction : 'None' !!}{!! $character->factionRank ? ' (' . $character->factionRank->name . ')' : null !!}</div>
+                    </div>
+                @endif
+                <div class="row">
+                    <div class="col-lg-5 col-md-6 col-4">
+                            <h5>Species</h5>
+                    </div>
+                    <div class="col-lg-7 col-md-6 col-8">{!! $image->species_id ? $image->species->displayName : 'None' !!}</div>
+                </div>
+
+			</p>
+		</div>
+	</div>
+</div>
+
+
     {{-- Info --}}
     <div class="card character-bio">
         <div class="card-header">
